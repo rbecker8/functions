625d33466ea11771120aabaee14568dc785e3965
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-16 15:27:51 -0600

    Added echo function to php file.

--- functions.php
+++ functions.php
@@ -9,5 +9,16 @@ function divide ($x, $y) {
 
 echo "60 / 128 = " . divide(60, 128 ) . " Gallons. <br>";
 echo "95 / 128 = " . divide(95, 128) . " Gallons.<br>";
-echo "578 / 128 = " . divide(578, 128) . " Gallons.";
+echo "578 / 128 = " . divide(578, 128) . " Gallons.<br><br>";
+
+
+function displayMsg() {
+	echo "Welcome to my functions page<br>";
+	echo "My name is Ryan<br>";
+	echo "and I'm a web developer in training!<br><br>";
+}
+
+displayMsg();
+
+
 
