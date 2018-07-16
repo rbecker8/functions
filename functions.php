4547c2d343af610c3d6fd242dd5b11eb3f36776a
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-16 14:54:07 -0600

    Added divide function to php file.

---
+++ functions.php
@@ -0,0 +1,13 @@
+
+<?php
+function divide ($x, $y) {
+	$z = $x / $y;
+	return $z;
+
+}
+
+
+echo "60 / 128 = " . divide(60, 128 ) . " Gallons. <br>";
+echo "95 / 128 = " . divide(95, 128) . " Gallons.<br>";
+echo "578 / 128 = " . divide(578, 128) . " Gallons.";
+
